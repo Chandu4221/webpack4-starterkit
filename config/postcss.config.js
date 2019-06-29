1f8b2eb3557bd049a52feabb5e2a2ad0c565b6c1
chandu4221 <chndraja@gmail.com>
2019-06-29 15:53:30 +0530

    Added PurgeCSS to postcss.config.js

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,3 +1,10 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const path = require("path");
 module.exports = {
-  plugins: [require("autoprefixer")]
+  plugins: [
+    require("autoprefixer"),
+    purgecss({
+      content: [`${path.resolve(__dirname, "../src")}/*.html`]
+    })
+  ]
 };
